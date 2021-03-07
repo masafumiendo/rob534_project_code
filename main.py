f11280e825a655e820791746bf319d35b4cda77f
SherlockY <37371094+HaonanYuan@users.noreply.github.com>
2021-03-07 02:10:19 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import numpy as np
+from RRG import RRG
+import math
+
+
+class GenerateMatrices:
+    def __init__(self, ij_matrix, ji_matrix):
+        self.ij_matrix = ij_matrix
+        self.ji_matrix = ji_matrix
 
 
 class error_propagation:
 
-    def __init__(self, vertex, edges):
-        self.vertex = vertex
-        self.edges = edges
+    def __init__(self, graph, map=None, wo_sig=None, vo_sig=None):
+        self.edges = graph.edges
+        self.vertices = graph.vertices
+        self.matrix = []
 
-        # define the true state
+        """# define the true state
         self.x_hat = []
         self.y_hat = []
         self.theta_hat = []
@@ -15,16 +24,11 @@ class error_propagation:
         # define the estimate state
         self.x = []
         self.y = []
-        self.theta = []
+        self.theta = []"""
 
-    # The estimate error propagation
+    """# The estimate error propagation
     def eep(self, j, i):
-        """
-        Calculate the estimate error propagation
-        :param j: to_vertex index
-        :param i: from_vertex index
-        :return:
-        """
+
 
     # The state estimate error
     def see(self):
@@ -54,20 +58,40 @@ class error_propagation:
         theta = theta * np.pi /180
         # create the rotation matrix 3x3,type = array
         r_matrix = np.array([[np.cos(theta), -np.sin(theta), 0],[np.sin(theta), np.cos(theta), 0],[0, 0, 1]])
-        return r_matrix
+        return r_matrix"""
 
     # The coefficient matrices
     def coefficient_matrix(self):
-        self.A_ij = np.array([[1, 0, (-self.edges.cost.hat.x*np.sin(self.edges.to_vertex.hat.theta*np.pi/180) - self.edges.cost.hat.y*np.cos(self.edges.to_vertex.hat.theta*np.pi/180))],
-                [0, 1, (self.edges.cost.hat.x*np.cos(self.edges.to_vertex.hat.theta*np.pi/180) - self.edges.cost.hat.y*np.sin(self.edges.to_vertex.hat.theta*np.pi/180))],
-                [0, 0, 1]])
-        self.B_ij = np.array([[np.cos(self.edges.to_vertex.hat.theta*np.pi/180), -np.sin(self.edges.to_vertex.hat.theta*np.pi/180), 0],
-                [np.sin(self.edges.to_vertex.hat.theta*np.pi/180), np.cos(self.edges.to_vertex.hat.theta*np.pi/180), 0],
-                [0, 0, 1]])
-        return self.A_ij, self.B_ij
+        for Edges in self.edges:
+            delta_x_ij = Edges.vertex_b[0] - Edges.vertex_a[0]
+            delta_y_ij = 0
+            delta_x_ji = Edges.vertex_a[0] - Edges.vertex_b[0]
+            delta_y_ji = 0
 
-    #The covariance matrix
-    def cov_matrix(self, initial_cov_g, initial_cov_b):
+            theta_ij = math.atan2(Edges.vertex_b[1] - Edges.vertex_a[1], Edges.vertex_b[0] - Edges.vertex_a[0])
+            theta_ji = math.atan2(Edges.vertex_a[1] - Edges.vertex_b[1], Edges.vertex_a[0] - Edges.vertex_b[0])
+
+            A_ij = np.array([[1, 0, -delta_x_ij * np.sin(theta_ij)],
+                             [0, 1, (delta_x_ij * np.cos(theta_ij))],
+                             [0, 0, 1]])
+            B_ij = np.array([[np.cos(theta_ij), -np.sin(theta_ij), 0],
+                             [np.sin(theta_ij), np.cos(theta_ij), 0],
+                             [0, 0, 1]])
+
+            A_ji = np.array([[1, 0, -delta_x_ji * np.sin(theta_ji)],
+                             [0, 1, (delta_x_ji * np.cos(theta_ji))],
+                             [0, 0, 1]])
+            B_ji = np.array([[np.cos(theta_ji), -np.sin(theta_ji), 0],
+                             [np.sin(theta_ji), np.cos(theta_ji), 0],
+                             [0, 0, 1]])
+
+            self.matrix.append(GenerateMatrices([A_ij, B_ij], [A_ji, B_ji]))
+
+        return self.matrix
+
+        # The covariance matrix
+
+    """def cov_matrix(self, initial_cov_g, initial_cov_b):
         # initial_cov_g = P_i in paper
         # P_i = [self.error_from_vertex.x.g, self.error_from_vertex.y.g, self.error_from_vertex.theta].T
         # initial_cov_b = Epsilon in paper
@@ -75,10 +99,16 @@ class error_propagation:
         # return the P_j
         # 3x3 * 3x1 * 3x3
         P_j = self.A_ij * np.cov(initial_cov_g) * self.A_ij.T + self.B_ij * initial_cov_b * self.B_ij.T
-        return P_j
-
-
+        return P_j"""
 
 
 if __name__ == '__main__':
+    graph = RRG(0, 1, 0, 1, 250, 0.15)
+    ep = error_propagation(graph)
+    matrix = ep.coefficient_matrix()
+    for item in matrix:
+        print(item.ij_matrix[0])
+        print(item.ij_matrix[1])
+        print(item.ji_matrix[0])
+        print(item.ji_matrix[1])
 
